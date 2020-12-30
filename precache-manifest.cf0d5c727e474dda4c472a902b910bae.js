54dba72941482bbe7d8e0ef586411dd7ea5dbf0a
Florencia Martina <florenciamartina15@gmail.com>
2020-12-30 16:34:50 +0800

    Updates

---
+++ precache-manifest.cf0d5c727e474dda4c472a902b910bae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6713b9e4261130bb3f08dea75f11a0f4",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "88f3ed55cc6cb7749ea1",
+    "url": "/movieapp/static/css/main.c27d3369.chunk.css"
+  },
+  {
+    "revision": "0a7738d725d2fed0faf2",
+    "url": "/movieapp/static/js/2.78a829b6.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/movieapp/static/js/2.78a829b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f3ed55cc6cb7749ea1",
+    "url": "/movieapp/static/js/main.446eb351.chunk.js"
+  },
+  {
+    "revision": "d6e3507a73551d0bdcd5",
+    "url": "/movieapp/static/js/runtime-main.a7dfa686.js"
+  },
+  {
+    "revision": "fb81c293e8cd99a0a10602ebd7bf9b53",
+    "url": "/movieapp/static/media/whale.fb81c293.png"
+  }
+]);
